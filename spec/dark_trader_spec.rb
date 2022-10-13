bde52542c6af58104e2c7587317f250b1e07ee67
GillianLEVERT <114432157+GillianLEVERT@users.noreply.github.com>
2022-10-13 13:43:53 +0200

    Last Push

--- spec/dark_trader_spec.rb
+++ spec/dark_trader_spec.rb
@@ -10,7 +10,7 @@ describe "crypto" do
     it "return the name of crypto" do  
      expect(crypto(scrapper)).to include {"BTC"}
      expect(crypto(scrapper)).to include {"ETH"}
-     expect(crypto(scrapper)).to include ("USDT")
+     expect(crypto(scrapper)).to include {"USDT"}
     end 
 
 end
